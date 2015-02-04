e07deb9f51fd52a25f9be0b0f25ddbb69d72d7fa
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-02-03 22:14:15 -0800

    testing string reading

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,7 +13,10 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofSetColor(255, 0, 0);
+    ofDrawBitmapString(ofToString(ofGetElapsedTimeMillis()), 20,ofGetHeight()-20);
+    ofSetColor(255);
+    
 }
 
 //--------------------------------------------------------------
@@ -42,7 +45,19 @@ void ofApp::mousePressed(int x, int y, int button){
     if (s == "") {
         cout << "zilch" << endl;
     } else {
-        cout << s << endl;
+        vector<string> pieces = ofSplitString(s, " ");
+        
+        unsigned long long millis;
+        if (ofIsStringInString(pieces[0], ":")) {
+            vector<string> timeStr = ofSplitString(pieces[0], ":");
+            int minutes = ofToInt(timeStr[0]);
+            int seconds = ofToInt(timeStr[1]);
+            millis = (unsigned long long)((seconds + (minutes * 60)) * 1000);
+        } else {
+            millis = (unsigned long long)(ofToInt(pieces[0]) * 1000);
+        }
+        
+        cout << millis << " | " << pieces[1] << endl;
     }
 }
 
